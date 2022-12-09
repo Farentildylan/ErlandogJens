71d11d1295f9b6b976436d42aeb40be7df5c8a1d
Erland <erland.handegard@outlook.com>
2022-12-09 09:53:45 +0100

    Oppgave 4 er ferdig klar for levering

--- oppgave4.py
+++ oppgave4.py
@@ -1,35 +1,45 @@
 import csv
 import matplotlib.pyplot as plt
-import numpy as np
 
 filnavn = "Skilsmisser og ekteskap.csv"
 
-yverdierekteskap = []
-yverdierskilsmisse = []
-xverdier = []
+class SortertData:
+    def __init__(self, filnavn):
+        self.filnavn = filnavn
+        self.yverdierekteskap = []
+        self.yverdierskilsmisse = []
+        self.xverdier = []
+        self.nyxverdier = []
 
-with open(filnavn, encoding="utf-8-sig") as fil:
-    filinnhold = csv.reader(fil, delimiter=";")
-    for _filinnhold in filinnhold: 
-        if _filinnhold[0] == "Inngåtte ekteskap":
-            yverdierekteskap.append(_filinnhold[1], _filinnhold[len(_filinnhold)])
-        elif _filinnhold[0] == "Skilsmisser":
-            yverdierskilsmisse.append(_filinnhold[1], _filinnhold[len(_filinnhold)])
-        else:
-            xverdier.append(_filinnhold[1], _filinnhold[len(_filinnhold)])
-
-"""
-    for i in filinnhold:
-        fil_linjer.append(i)
-
-    xverdier = np.arange(1902,2022)
-    yverdierekteskap = fil_linjer[2]
-    yverdierekteskap.pop(0)
-    yverdierskilsmisse = fil_linjer[3]
-    yverdierekteskap.pop(0)
-
-"""
-
-plt.bar(xverdier + 0.2, yverdierekteskap, width=0.5)
-plt.bar(xverdier - 0.2, yverdierskilsmisse, width=0.5)
-plt.show()
+        with open(self.filnavn, encoding="utf-8-sig") as fil:
+            filinnhold = csv.reader(fil, delimiter=";")
+            for _filinnhold in filinnhold: 
+                if _filinnhold[0] == "Inngåtte ekteskap":
+                    _filinnhold.pop(0)
+                    self.yverdierekteskap = _filinnhold
+                elif _filinnhold[0] == "Skilsmisser":
+                    _filinnhold.pop(0)
+                    self.yverdierskilsmisse = _filinnhold
+                elif _filinnhold[0] == "statistikkvariabel":
+                    _filinnhold.pop(0)
+                    self.xverdier = _filinnhold
+    
+    def konverter(self):
+        self.xverdier = [int(x) for x in self.xverdier]
+        self.nyxverdier = self.xverdier
+        self.xverdier = [x - 2 for x in self.xverdier]
+        self.nyxverdier = [x + 2 for x in self.nyxverdier]
+        self.yverdierekteskap = [int(x) for x in self.yverdierekteskap]
+        self.yverdierskilsmisse = [int(x) for x in self.yverdierskilsmisse]
+    
+    def skrivUt(self):
+        plt.bar(self.xverdier, self.yverdierekteskap, width=4)
+        plt.bar(self.nyxverdier, self.yverdierskilsmisse, width=4)
+        plt.xlabel("Antall år")
+        plt.ylabel("Skilsmisser og Inngåttekteskap")
+        plt.legend(["Ingåtte ekteskap","Skilsmisser"])
+        plt.show()
+        
+sortertData = SortertData(filnavn)
+sortertData.konverter()
+sortertData.skrivUt()
